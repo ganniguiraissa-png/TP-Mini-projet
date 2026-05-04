2bcc4f0ce9254b407ece75849925906337f0bd03
Salsabil <salsabil.cajee09@gmail.com>
2026-05-04 16:18:57 +0200

    Rendu final

--- script1.js
+++ script1.js
@@ -5,7 +5,7 @@ let tasks = [
 } 
 ]; 
 
-document.getElementById("bouton1").addEventListener("click", () => {
+document.getElementById("bouton0").addEventListener("click", () => {
     const nouvelleTache = prompt("Entrez une nouvelle tâche :");
     if (nouvelleTache && nouvelleTache.trim() !== "") {
         const li = document.createElement("li");
@@ -28,3 +28,35 @@ document.getElementById("bouton2").addEventListener("click", () => {
         alert("Numéro invalide");
     }
 });
+
+    document.getElementById("bouton1").addEventListener("click", () => {
+    const liste = document.getElementById("liste");
+    const items = liste.getElementsByTagName("li");
+
+    if (items.length === 0) {
+        alert("Aucune tâche disponible");
+        return;
+    }
+    const numero = prompt("Numéro de la tâche à décrire :");
+    const index = parseInt(numero) - 1;
+
+    if (index < 0 || index >= items.length) {
+        alert("Numéro invalide");
+        return;
+    }
+
+    const description = prompt("Entrez la description :");
+
+    if (!description || description.trim() === "") {
+        return;
+    }
+
+      const p = document.createElement("p");
+    p.innerText = "Description : " + description;
+    p.style.marginTop = "5px";
+    p.style.fontSize = "14px";
+    p.style.color = "#555";
+
+    items[index].appendChild(p);
+
+    });
