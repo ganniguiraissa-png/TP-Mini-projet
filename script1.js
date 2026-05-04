7391f410a3e0ce96d5c82e62c392092553bc9d32
Salsabil <salsabil.cajee09@gmail.com>
2026-05-04 14:14:01 +0200

    Modif du script, ajt de tâches fonctionnel

--- script1.js
+++ script1.js
@@ -1,8 +1,16 @@
 let tasks = [ 
 { 
-id: 1, 
-title: "Faire la page d'accueil", 
-description: "Créer le code HTML et la maquette du site web", 
-done: false 
+    id: 1, 
+    title: "Faire la page d'accueil", 
 } 
 ]; 
+
+document.getElementById("bouton1").addEventListener("click", () => {
+    const nouvelleTache = prompt("Entrez une nouvelle tâche :");
+    if (nouvelleTache && nouvelleTache.trim() !== "") {
+        const li = document.createElement("li");
+        li.innerText = nouvelleTache;
+        document.getElementById("liste").appendChild(li);
+    }
+});
+
